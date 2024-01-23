43e531387468e9c2bc48f2dfd16123a9daa983d6
Ed Williams <edwilliamsdevelopment@gmail.com>
2024-01-23 08:32:13 +0000

    fix: move from CRA to Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { usePlugin } from './extism.tsx' // '@extism/react' is broken
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 const pluginSource = `${window.location.origin}/greet.wasm`
 
@@ -7,19 +7,19 @@ function App() {
   const [hookInput, setHookInput] = useState('')
   const [str, setStr] = useState('')
   const {
-    // plugin,
-    // loading,
-    useFunction,
+    plugin,
+    loading,
+    // useFunction,
   } = usePlugin(pluginSource, { useWasi: true })
 
-  const { output, loading } = useFunction('greet', hookInput)
+  // const { output, loading } = useFunction('greet', hookInput)
 
   useEffect(() => {
-    setStr(new TextDecoder().decode(output))
-    //   plugin?.call('greet', hookInput).then((output) => {
-    //     setStr(new TextDecoder().decode(output))
-    //   })
-  }, [output, hookInput])
+    // setStr(new TextDecoder().decode(output))
+    plugin?.call('greet', hookInput).then((output: any) => {
+      setStr(new TextDecoder().decode(output))
+    })
+  }, [plugin, hookInput])
 
   return (
     <div>
