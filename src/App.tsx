2c85782dcb500976f3463d351d7780e86ddaed33
Ed Williams <edwilliamsdevelopment@gmail.com>
2024-01-27 20:17:04 +0000

    feat: game complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,57 @@
 import { usePlugin } from './extism.tsx'
 import { useState, useEffect } from 'react'
-// import Game from './Game'
+import Game from './Game.tsx'
 import './style.css'
 
+// todo: pass string to callWasm and remove input altogether
+
+const initialState = {
+  board: [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ],
+  row: 0,
+  col: 0,
+  player: '',
+}
+
 function App() {
-  // return <Game />
-  const [input, setInput] = useState({
-    board: [
-      ['', '', ''],
-      ['', '', ''],
-      ['', '', ''],
-    ],
-    row: 0,
-    col: 0,
-    player: 'X',
-  })
-  const [str, setStr] = useState('')
   const { plugin, loading } = usePlugin(`${window.location.origin}/app.wasm`, {
     useWasi: true,
   })
 
+  // input is an object, which is stringified and sent to WASM
+  const [input, setInput] = useState(initialState)
+
+  // output is the stringified object that comes back from WASM
+  const [output, setOutput] = useState('')
+  const [lastPlayer, setLastPlayer] = useState('X')
+
+  // whenever input is changed, WASM is called which then returns a new output
   useEffect(() => {
-    const _input = JSON.stringify(input)
-    // console.log(_input)
-    plugin?.call('app', _input).then((output: any) => {
-      setStr(new TextDecoder().decode(output))
-    })
+    const main = async () => {
+      const str = await callWasm()
+      setOutput(str)
+    }
+    main()
   }, [plugin, input])
+
+  // this sends our stringified input to WASM and returns a new stringified object
+  const callWasm = async () => {
+    const val: any = await plugin?.call('app', JSON.stringify(input))
+    return new TextDecoder().decode(val)
+  }
+
+  const handleClick = ({ row, col }: { row: number; col: number }) => {
+    const player = lastPlayer === 'X' ? 'O' : 'X'
+    setInput({ board: display.board, row, col, player })
+    setLastPlayer(player)
+  }
+
+  const display = output === '' ? initialState : JSON.parse(output)
+  const winner = JSON.stringify(display.winner)
+  const currentPlayer = lastPlayer === 'X' ? 'O' : 'X'
 
   return (
     <div>
@@ -34,14 +59,12 @@ function App() {
         <p>Loading wasm...</p>
       ) : (
         <div>
-          <input
-            className="input"
-            value={JSON.stringify(input)}
-            onChange={(e) => setInput(JSON.parse(e.target.value))}
-          />
-          <br />
-          <br />
-          {str}
+          <Game board={display.board} onClick={handleClick} />
+          {winner === 'null' ? (
+            <h3>It is {currentPlayer} players turn</h3>
+          ) : (
+            <h1>Winner: {winner}</h1>
+          )}
         </div>
       )}
     </div>
--- src/Game.tsx
+++ src/Game.tsx
@@ -1,20 +1,44 @@
-function Game() {
+function Game({
+  board,
+  onClick,
+}: {
+  board: string[][]
+  onClick: (val: any) => void
+}) {
   return (
-    <div>
-      <div className="float">
-        <div className="square">1</div>
-        <div className="square">2</div>
-        <div className="square">3</div>
+    <div className="flex">
+      <div className="flex-col">
+        <div onClick={() => onClick({ row: 0, col: 0 })} className="square">
+          {board[0][0]}
+        </div>
+        <div onClick={() => onClick({ row: 1, col: 0 })} className="square">
+          {board[1][0]}
+        </div>
+        <div onClick={() => onClick({ row: 2, col: 0 })} className="square">
+          {board[2][0]}
+        </div>
       </div>
-      <div className="float">
-        <div className="square">4</div>
-        <div className="square">5</div>
-        <div className="square">6</div>
+      <div className="flex-col">
+        <div onClick={() => onClick({ row: 0, col: 1 })} className="square">
+          {board[0][1]}
+        </div>
+        <div onClick={() => onClick({ row: 1, col: 1 })} className="square">
+          {board[1][1]}
+        </div>
+        <div onClick={() => onClick({ row: 2, col: 1 })} className="square">
+          {board[2][1]}
+        </div>
       </div>
-      <div className="float">
-        <div className="square">7</div>
-        <div className="square">8</div>
-        <div className="square">9</div>
+      <div className="flex-col">
+        <div onClick={() => onClick({ row: 0, col: 2 })} className="square">
+          {board[0][2]}
+        </div>
+        <div onClick={() => onClick({ row: 1, col: 2 })} className="square">
+          {board[1][2]}
+        </div>
+        <div onClick={() => onClick({ row: 2, col: 2 })} className="square">
+          {board[2][2]}
+        </div>
       </div>
     </div>
   )
