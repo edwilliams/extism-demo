fd159823dbd65a5c67f3a0b3ba7e96d3491aa4ba
Ed Williams <edwilliamsdevelopment@gmail.com>
2024-01-29 20:00:26 +0000

    refactor app.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,25 @@
 import { usePlugin } from './extism.tsx'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Game from './Game.tsx'
 import './style.css'
-
-const initialState = {
-  board: [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
-  ],
-  row: 0,
-  col: 0,
-  player: '',
-}
 
 function App() {
   const { plugin, loading } = usePlugin(`${window.location.origin}/app.wasm`, {
     useWasi: true,
   })
 
-  // output is the stringified object that comes back from WASM
-  const [output, setOutput] = useState('')
+  const [state, setState] = useState({
+    winner: '',
+    board: [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ],
+    row: 0,
+    col: 0,
+    player: '',
+  })
   const [lastPlayer, setLastPlayer] = useState('X')
-
-  // whenever input is changed, WASM is called which then returns a new output
-  useEffect(() => {
-    const main = async () => {
-      const str = await callWasm(JSON.stringify(initialState))
-      setOutput(str)
-    }
-    main()
-  }, [plugin])
 
   // this sends our stringified input to WASM and returns a new stringified object
   const callWasm = async (str: string) => {
@@ -40,14 +29,13 @@ function App() {
 
   const handleClick = async ({ row, col }: { row: number; col: number }) => {
     const player = lastPlayer === 'X' ? 'O' : 'X'
-    const input = { board: display.board, row, col, player }
+    const input = { board: state.board, row, col, player }
     const str = await callWasm(JSON.stringify(input))
-    setOutput(str)
+    setState(JSON.parse(str))
     setLastPlayer(player)
   }
 
-  const display = output === '' ? initialState : JSON.parse(output)
-  const winner = JSON.stringify(display.winner)
+  const winner = JSON.stringify(state.winner)
   const currentPlayer = lastPlayer === 'X' ? 'O' : 'X'
 
   return (
@@ -56,11 +44,11 @@ function App() {
         <p>Loading wasm...</p>
       ) : (
         <div>
-          <Game board={display.board} onClick={handleClick} />
-          {winner === 'null' ? (
-            <h3>It is {currentPlayer} players turn</h3>
-          ) : (
+          <Game board={state.board} onClick={handleClick} />
+          {winner === '"O"' || winner === '"X"' ? (
             <h1>Winner: {winner}</h1>
+          ) : (
+            <h3>It is {currentPlayer} players turn</h3>
           )}
         </div>
       )}
